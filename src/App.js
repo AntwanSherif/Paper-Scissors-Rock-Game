70b56352bbb785254276d4f66175b7e863982628
AntwanSherif <antwansherif@gmail.com>
2020-04-10 21:32:38 +0200

    refactor: Update HTML head, package.json, manifest & static assets

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      Score
+      Rules
+    
+      You Picked
+      The House Picked
+    
+      You Win
+      You Lose
+    
+      Play Again
       </header>
+
+      <footer className='app-footer'>
+        <div class="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noopener noreferrer">Frontend Mentor</a>.
+          Coded by <a href="#">Antwan Sherif</a>.
+        </div>
+      </footer>
     </div>
   );
 }
